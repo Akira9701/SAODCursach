4618deaa7df56c5a4265b92d75e7cdf8ae8dc348
Akira <akira.zzz97@gmail.com>
2023-09-11 01:18:38 +0700

    feature: add sort by title and sum

--- main.cpp
+++ main.cpp
@@ -23,69 +23,60 @@ void swapRecords(record2 &a, record2 &b)
 	b = temp;
 }
 
-// Пирамидальная сортировка по полю 'a'
-void heapifyByFieldA(record2 arr[], int n, int i)
+// Сравнение двух записей по сложному ключу
+int compareRecords(record2 &a, record2 &b)
+{
+	// Сравнение по первым 3 символам поля 'a'
+	int cmp = strncmp(a.a, b.a, 3);
+
+	// Если первые 3 символа одинаковы, сравнение по полю 'b'
+	if (cmp == 0)
+	{
+		if (a.b < b.b)
+			return -1;
+		else if (a.b > b.b)
+			return 1;
+		else
+			return 0;
+	}
+
+	return cmp;
+}
+
+// Построение кучи по сложному ключу
+void heapifyByComplexKey(record2 arr[], int n, int i)
 {
 	int largest = i;
 	int l = 2 * i + 1;
 	int r = 2 * i + 2;
 
-	if (l < n && strcmp(arr[l].a, arr[largest].a) > 0)
+	// Используем функцию compareRecords для сравнения
+	if (l < n && compareRecords(arr[l], arr[largest]) > 0)
 		largest = l;
 
-	if (r < n && strcmp(arr[r].a, arr[largest].a) > 0)
+	if (r < n && compareRecords(arr[r], arr[largest]) > 0)
 		largest = r;
 
 	if (largest != i)
 	{
 		swapRecords(arr[i], arr[largest]);
-		heapifyByFieldA(arr, n, largest);
+		heapifyByComplexKey(arr, n, largest);
 	}
 }
 
-void heapSortByFieldA(record2 arr[], int n)
+// Сортировка кучей по сложному ключу
+void heapSortByComplexKey(record2 arr[], int n)
 {
 	for (int i = n / 2 - 1; i >= 0; i--)
-		heapifyByFieldA(arr, n, i);
+		heapifyByComplexKey(arr, n, i);
 
 	for (int i = n - 1; i >= 0; i--)
 	{
 		swapRecords(arr[0], arr[i]);
-		heapifyByFieldA(arr, i, 0);
+		heapifyByComplexKey(arr, i, 0);
 	}
 }
 
-// Пирамидальная сортировка по полю 'b' (от большего к меньшему)
-void heapifyByFieldB(record2 arr[], int n, int i)
-{
-	int largest = i;
-	int l = 2 * i + 1;
-	int r = 2 * i + 2;
-
-	if (l < n && arr[l].b > arr[largest].b)
-		largest = l;
-
-	if (r < n && arr[r].b > arr[largest].b)
-		largest = r;
-
-	if (largest != i)
-	{
-		swapRecords(arr[i], arr[largest]);
-		heapifyByFieldB(arr, n, largest);
-	}
-}
-
-void heapSortByFieldB(record2 arr[], int n)
-{
-	for (int i = n / 2 - 1; i >= 0; i--)
-		heapifyByFieldB(arr, n, i);
-
-	for (int i = n - 1; i >= 0; i--)
-	{
-		swapRecords(arr[0], arr[i]);
-		heapifyByFieldB(arr, i, 0);
-	}
-}
 int main()
 {
 	int counter = -20;
@@ -98,90 +89,57 @@ int main()
 	i = fread((record2 *)mas2, sizeof(record2), 4000, fp);
 	i = 0;
 
-	cout << "Choose sorting field (name or sum) name/sum: ";
-	cin >> word;
-
-	if (word == "name")
-	{
-		heapSortByFieldA(mas2, 4000); // Сортировка по полю 'a'
-	}
-	else if (word == "sum")
-	{
-		heapSortByFieldB(mas2, 4000); // Сортировка по полю 'b'
-	}
-	else
-	{
-		cerr << "Invalid sorting field." << endl;
-		return 1;
-	}
-
 	cout << "Show list of users? (Y/N)" << endl;
 	cin >> word;
 
 	if (word == "y")
 	{
+		cout << "Sort y/n ?" << endl;
+		cin >> word;
+		if (word == "y")
+		{
+			heapSortByComplexKey(mas2, 4000); // Сортировка по сложному ключу
+		}
 
 		while (counter < 4000)
 		{
 
-			if (word == "y" || word == "d")
+			if (word == "y" || word == "n" || word == "d")
 			{
 				counter += 20;
 				maxPage += 20;
-				cout << "\033[2J\033[1;1H";
-
-				cout << "Number"
-					 << "\t"
-					 << "Name"
-					 << "\t\t\t\t"
-					 << "Dolg"
-					 << "\t"
-					 << "Date"
-					 << "\t\t"
-					 << "Lowyer" << endl;
-				for (int i = counter; i < maxPage; i++)
-				{
-					cout << i + 1 << "\t" << mas2[i].a << "\t" << mas2[i].b << "\t" << mas2[i].c << "\t" << mas2[i].d << endl;
-				}
-
-				cout << endl;
-				cout << "Prev: a" << endl;
-				cout << "Next: d" << endl;
-				cout << "End: r" << endl;
-				cin >> word;
 			}
 			else if (word == "a")
 			{
 				counter -= 20;
 				maxPage -= 20;
-
-				cout << "\033[2J\033[1;1H";
-
-				cout << "Number"
-					 << "\t"
-					 << "Name"
-					 << "\t\t\t\t"
-					 << "Dolg"
-					 << "\t"
-					 << "Date"
-					 << "\t\t"
-					 << "Lowyer" << endl;
-				for (int i = counter; i < maxPage; i++)
-				{
-					cout << i + 1 << "\t" << mas2[i].a << "\t" << mas2[i].b << "\t" << mas2[i].c << "\t" << mas2[i].d << endl;
-				}
-
-				cout << endl;
-				cout << "Prev: a" << endl;
-				cout << "Next: d" << endl;
-				cout << "End: r" << endl;
-				cin >> word;
 			}
 			else
 			{
 				exit(0);
 				return 0;
 			}
+			cout << "\033[2J\033[1;1H";
+
+			cout << "Number"
+				 << "\t"
+				 << "Name"
+				 << "\t\t\t\t"
+				 << "Dolg"
+				 << "\t"
+				 << "Date"
+				 << "\t\t"
+				 << "Lowyer" << endl;
+			for (int i = counter; i < maxPage; i++)
+			{
+				cout << i + 1 << "\t" << mas2[i].a << "\t" << mas2[i].b << "\t" << mas2[i].c << "\t" << mas2[i].d << endl;
+			}
+
+			cout << endl;
+			cout << "Prev: a" << endl;
+			cout << "Next: d" << endl;
+			cout << "End: r" << endl;
+			cin >> word;
 		}
 	}
 
