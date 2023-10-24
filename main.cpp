bddbb80508ad5ca1aec8da4d83be61a02f533816
Akira <antonov.egor@yandex.ru>
2023-10-24 01:40:11 -0700

    feature: have ability to find

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <string>
 #include <cctype>
 #include <regex>
+#include <queue>
 
 using namespace std;
 
@@ -17,7 +18,6 @@ struct record2
 	char d[22];
 };
 
-// Функция для обмена двух записей
 void swapRecords(record2 &a, record2 &b)
 {
 	record2 temp = a;
@@ -25,13 +25,9 @@ void swapRecords(record2 &a, record2 &b)
 	b = temp;
 }
 
-// Сравнение двух записей по сложному ключу
 int compareRecords(record2 &a, record2 &b)
 {
-	// Сравнение по первым 3 символам поля 'a'
 	int cmp = strncmp(a.a, b.a, 30);
-
-	// Если первые 3 символа одинаковы, сравнение по полю 'b'
 	if (cmp == 0)
 	{
 		if (a.b < b.b)
@@ -41,122 +37,180 @@ int compareRecords(record2 &a, record2 &b)
 		else
 			return 0;
 	}
-
 	return cmp;
 }
 
-// Построение кучи по сложному ключу
-void heapifyByComplexKey(record2 arr[], int n, int i)
+void heapifyByComplexKey(record2 arr[], int index[], int n, int i)
 {
 	int largest = i;
 	int l = 2 * i + 1;
 	int r = 2 * i + 2;
-
-	// Используем функцию compareRecords для сравнения
-	if (l < n && compareRecords(arr[l], arr[largest]) > 0)
+	if (l < n && compareRecords(arr[index[l]], arr[index[largest]]) > 0)
 		largest = l;
-
-	if (r < n && compareRecords(arr[r], arr[largest]) > 0)
+	if (r < n && compareRecords(arr[index[r]], arr[index[largest]]) > 0)
 		largest = r;
-
 	if (largest != i)
 	{
-		swapRecords(arr[i], arr[largest]);
-		heapifyByComplexKey(arr, n, largest);
+		swap(index[i], index[largest]);
+		heapifyByComplexKey(arr, index, n, largest);
 	}
 }
 
-// Сортировка кучей по сложному ключу
-void heapSortByComplexKey(record2 arr[], int n)
+void heapSortByComplexKey(record2 arr[], int index[], int n)
 {
 	for (int i = n / 2 - 1; i >= 0; i--)
-		heapifyByComplexKey(arr, n, i);
-
+		heapifyByComplexKey(arr, index, n, i);
 	for (int i = n - 1; i >= 0; i--)
 	{
-		swapRecords(arr[0], arr[i]);
-		heapifyByComplexKey(arr, i, 0);
+		swap(index[0], index[i]);
+		heapifyByComplexKey(arr, index, i, 0);
 	}
 }
 
 bool isNumber(const std::string &str)
 {
-	// Регулярное выражение, которое соответствует целым числам (с или без знака)
 	std::regex pattern("^[-+]?\\d+$");
-
-	// Проверяем, соответствует ли строка регулярному выражению
 	return std::regex_match(str, pattern);
+}
+
+void displayRecords(record2 arr[], int index[], int start, int end, int i)
+{
+	cout << "Number"
+		 << "\t"
+		 << "Name"
+		 << "\t\t\t\t"
+		 << "Dolg"
+		 << "\t"
+		 << "Date"
+		 << "\t\t"
+		 << "Lowyer" << endl;
+	for (int j = start; j < end && j < i; j++)
+	{
+		int recordIndex = index[j];
+		cout << j + 1 << "\t" << arr[recordIndex].a << "\t" << arr[recordIndex].b << "\t" << arr[recordIndex].c << "\t" << arr[recordIndex].d << endl;
+	}
+	cout << endl;
+	cout << "Prev: p" << endl;
+	cout << "Next: n" << endl;
+	cout << "Any page: number" << endl;
+	cout << "Search: s" << endl;
+	cout << "Return to Menu: m" << endl;
 }
 
 int main()
 {
-	int counter = -20;
-	int maxPage = 0;
+	int counter = 0;
+	int maxPage = 20;
 	std::string word;
+	bool sorted = false; // Flag to track if the list is sorted
 	FILE *fp;
 	fp = fopen("file.dat", "rb");
 	record2 mas2[4000] = {0};
+	int index[4000] = {0}; // Index array to maintain the order
 	int i = 0;
 	i = fread((record2 *)mas2, sizeof(record2), 4000, fp);
-	i = 0;
+	fclose(fp);
 
-	cout << "Show list of users? (Y/N)" << endl;
-	cin >> word;
-
-	if (word == "y")
+	// Initialize the index array
+	for (int j = 0; j < i; j++)
 	{
-		cout << "Sort y/n ?" << endl;
+		index[j] = j;
+	}
+
+	while (true)
+	{
+		cout << "Main Menu:" << endl;
+		cout << "a. Show sorted list" << endl;
+		cout << "b. Show unsorted list" << endl;
+		cout << "c. Search by key" << endl;
+		cout << "d. Exit" << endl;
 		cin >> word;
-		if (word == "y")
+
+		if (word == "a")
 		{
-			heapSortByComplexKey(mas2, 4000); // Сортировка по сложному ключу
+			sorted = true;
+		}
+		else if (word == "b")
+		{
+			sorted = false;
+		}
+		else if (word == "c")
+		{
+			cout << "Enter a search key (3 characters) or 'q' to quit: ";
+			cin >> word;
+			if (word == "q" || word == "Q")
+			{
+				continue;
+			}
+
+			if (word.length() != 3)
+			{
+				cout << "Invalid search key length. It must be 3 characters." << endl;
+				continue;
+			}
+
+			cout << "Matching Records:" << endl;
+			queue<record2> matchingRecords;
+
+			for (int j = 0; j < i; j++)
+			{
+				if (strncmp(mas2[index[j]].a, word.c_str(), 3) == 0)
+				{
+					matchingRecords.push(mas2[index[j]]);
+				}
+			}
+			cout
+				<< "Name"
+				<< "\t\t\t\t"
+				<< "Dolg"
+				<< "\t"
+				<< "Date"
+				<< "\t\t"
+				<< "Lowyer" << endl;
+			while (!matchingRecords.empty())
+			{
+				record2 record = matchingRecords.front();
+				matchingRecords.pop();
+
+				cout << record.a << "\t" << record.b << "\t" << record.c << "\t" << record.d << endl;
+			}
+			cout << "Press m to go back...";
+			cin >> word;
+		}
+		else if (word == "d")
+		{
+			break;
 		}
 
-		while (counter < 4000)
+		if (sorted)
 		{
+			heapSortByComplexKey(mas2, index, i);
+		}
 
-			if (word == "y" || word == "n" || word == "d")
+		while (counter < i)
+		{
+			if (word == "n")
 			{
 				counter += 20;
 				maxPage += 20;
 			}
-			else if (word == "a")
+			else if (word == "p")
 			{
 				counter -= 20;
 				maxPage -= 20;
 			}
 			else if (isNumber(word))
 			{
-
 				counter = std::stoi(word) * 20;
 				maxPage = 20 + std::stoi(word) * 20;
 			}
-			else
+			else if (word == "m" || word == "M")
 			{
-				exit(0);
-				return 0;
-			}
-			cout << "\033[2J\033[1;1H";
-
-			cout << "Number"
-				 << "\t"
-				 << "Name"
-				 << "\t\t\t\t"
-				 << "Dolg"
-				 << "\t"
-				 << "Date"
-				 << "\t\t"
-				 << "Lowyer" << endl;
-			for (int i = counter; i < maxPage; i++)
-			{
-				cout << i + 1 << "\t" << mas2[i].a << "\t" << mas2[i].b << "\t" << mas2[i].c << "\t" << mas2[i].d << endl;
+				break;
 			}
 
-			cout << endl;
-			cout << "Prev: a" << endl;
-			cout << "Next: d" << endl;
-			cout << "Any page: number" << endl;
-			cout << "End: r" << endl;
+			cout << "\033[2J\033[1;1H"; // Clears the console screen
+			displayRecords(mas2, index, counter, maxPage, i);
 			cin >> word;
 		}
 	}
